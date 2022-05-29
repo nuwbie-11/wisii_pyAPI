0d169456ccda60a7d667270301729b85c91fca09
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-05-29 22:52:27 +0700

    mmm

--- controllers/main.py
+++ controllers/main.py
@@ -1,3 +1,4 @@
+import base64
 from flask import render_template, request, jsonify
 import numpy as np
 
@@ -5,4 +6,7 @@ import numpy as np
 
 def main():
     if request.method == 'POST':
-        return jsonify({'response':'Hello World'})
+        data = request.get_json(force=True)
+        image_data = data['image']
+        imgdata = base64.b64decode(image_data)
+        return jsonify({'response':f'Hello World{image_data}'})

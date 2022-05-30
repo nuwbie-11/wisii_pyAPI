8194f1bbd098c708219739873f08a2d76e74ce92
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-05-31 00:55:03 +0700

    trying with another method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template,request, jsonify
 # import joblib
 import numpy as np
+import io
 import cv2
 # from routes.route import user_bp
 
@@ -14,11 +15,20 @@ def predict():
         # data = request.get_json(force=True)
         # image_data = data['images']
         # imgdata = base64.b64decode(image_data)
-        img_file = request.files['images'].read()
+        img_file = request.files['images']
         # npimg = np.fromstring(img_file,np.uint8)
+        in_memory_file = io.BytesIO()
+        img_file.save(in_memory_file)
+        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+        color_image_flag = 1
+        img = cv2.imdecode(data, color_image_flag)
         # filename = werkzeug.utils.secure_filename(img_file.filename)\
         # img = cv2.imdecode(npimg, cv2.CV_LOAD_IMAGE_UNCHANGED)
-        return jsonify({'response':f'Hello World{img_file}'})
+        try:
+            return jsonify({'response':f'Hello World : {img.shape}'})
+        except Exception as e:
+            return jsonify({'response':f'Hello World : {e}'})
+        
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True,host = '0.0.0.0')

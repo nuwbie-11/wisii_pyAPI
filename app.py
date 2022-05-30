f4130a76f9d29290db65bdb145142fde2fa6486c
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-05-30 08:33:33 +0700

    Changes

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
-import joblib
-import numpy as np
-from routes.route import user_bp
+from flask import Flask, render_template,request, jsonify
+# import joblib
+# import numpy as np
+# from routes.route import user_bp
 
 app = Flask(__name__)
-app.register_blueprint(user_bp, url_prefix='/')
+# app.register_blueprint(user_bp, url_prefix='/')
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    if request.method == 'POST':
+
+        # data = request.get_json(force=True)
+        # image_data = data['image']
+        # imgdata = base64.b64decode(image_data)
+        # img_file = request.files['image']
+        # filename = werkzeug.utils.get_filename(img_file)
+        return jsonify({'response':'Hello World'})
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True,host = '0.0.0.0')

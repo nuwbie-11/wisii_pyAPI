c6ebbae8d5d2617592feebe72d02c7fa2cbae2e0
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-05-31 00:59:32 +0700

    Getting where is the error

--- app.py
+++ app.py
@@ -16,15 +16,16 @@ def predict():
         # image_data = data['images']
         # imgdata = base64.b64decode(image_data)
         img_file = request.files['images']
+        try:
         # npimg = np.fromstring(img_file,np.uint8)
-        in_memory_file = io.BytesIO()
-        img_file.save(in_memory_file)
-        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
-        color_image_flag = 1
-        img = cv2.imdecode(data, color_image_flag)
+            in_memory_file = io.BytesIO()
+            img_file.save(in_memory_file)
+            data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
+            color_image_flag = 1
+            img = cv2.imdecode(data, color_image_flag)
         # filename = werkzeug.utils.secure_filename(img_file.filename)\
         # img = cv2.imdecode(npimg, cv2.CV_LOAD_IMAGE_UNCHANGED)
-        try:
+        
             return jsonify({'response':f'Hello World : {img.shape}'})
         except Exception as e:
             return jsonify({'response':f'Hello World : {e}'})

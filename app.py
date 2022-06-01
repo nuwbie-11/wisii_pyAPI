8d5f9b4d9712c9baec5649f27b674b28e86811f7
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-06-01 23:42:44 +0700

    Lets Try Shall we

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import numpy as np
 import io
 import cv2
 # from routes.route import user_bp
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 # app.register_blueprint(user_bp, url_prefix='/')
@@ -11,12 +13,28 @@ app = Flask(__name__)
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
+        message = "Tidak Dikenali"
+        model = keras.load_model('model/MDClassification')
         img = request.files['images'].read()
         npimg = np.fromstring(img, np.uint8)
         img = cv2.imdecode(npimg,-1)
+        img = cv2.resize(img,(150,150))
+
+        x = image.img_to_array(img)
+        x = np.expand_dims(x,axis=0)
+
+        img = np.vstack([x])
+        classes = model.predict(img,batch_size=10)
+
+        if classes[0][0]==1:
+            message = ('100k')
+        elif classes[0][1]==1:
+            message = ('10k')
+        elif classes[0][2]==1:
+            message = ('50k')
 
         try:
-            return(jsonify({"response":f"Hellow{img.shape}"}))
+            return(jsonify({"response":f"{message}"}))
         except Exception as e:
             return(jsonify({"response":f"Hellow{e}"}))
 

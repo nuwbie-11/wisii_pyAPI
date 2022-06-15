3e92d3083695e5a500e01a8f59e77d86c7dca537
Roxasha <63080447+nuwbie-11@users.noreply.github.com>
2022-06-15 08:14:00 +0700

    Changes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
         x = image.img_to_array(img)
         x.resize(1, 224, 224, 3)
 
-        interpreter = tf.lite.Interpreter("model/tmp49xrx4h3.tflite")
+        interpreter = tf.lite.Interpreter("model/Mobilenetv2plusConv2d2V3.tflite")
         interpreter.allocate_tensors()
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
@@ -29,8 +29,8 @@ def predict():
         interpreter.invoke()
         output_data = interpreter.get_tensor(output_details[0]["index"])
         # print(output_data)
-        higehst_conf = max(output_data[0])
-        ix = np.where(output_data == higehst_conf)[-1][0]
+        # higehst_conf = max(output_data[0])
+        # ix = np.where(output_data == higehst_conf)[-1][0]
 
         labels = {
             0:"1.000",
@@ -42,10 +42,20 @@ def predict():
             6:"50.000",
         }
 
-        message = labels[ix]
+        result = f"""
+        1.000 Rupiah {(output_data[0][0]*100):.2f} %
+        2.000 Rupiah {(output_data[0][3]*100):.2f} %
+        5.000 Rupiah {(output_data[0][5]*100):.2f} %
+        10.000 Rupiah {(output_data[0][1]*100):.2f} %
+        20.000 Rupiah {(output_data[0][4]*100):.2f} %
+        50.000 Rupiah {(output_data[0][6]*100):.2f} %
+        100.000 Rupiah {(output_data[0][2]*100):.2f} %
+        """
+
+        # message = labels[ix]
 
         try:
-            return f"Prediksi {message} Rupiah Dengan nilai {(higehst_conf*100):.2f}%"
+            return result
         except Exception as e:
             return f"{e}"
 
